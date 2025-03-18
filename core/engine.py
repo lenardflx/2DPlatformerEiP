2085dda24b87c1079f97878ac1493bde6c70f316
Lönard <lenard.felix@outlook.com>
2025-03-18 11:22:48 +0100

    animated background, nbt blocks, level loader

--- core/engine.py
+++ core/engine.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame
 
 from core.camera import Camera
@@ -17,15 +19,31 @@ class GameEngine:
         pygame.display.set_caption(get_game_data("game_title"))
 
         self.clock = pygame.time.Clock()
-        w, h = get_game_data("player_size")
-        s = get_game_data("player_scale")
-        self.player = Player(50, 50, w, h, s)
-        self.background = Background()
-        self.level = Level(0)
+
+        with open("assets/background/background.json") as f:
+            data = json.load(f)
+        self.backgrounds = [Background(d) for d in data[::-1]]
+
+        self.current_level = 0
+        self.level = Level(self.current_level)
+
+        self.player = self.load_player()
 
         self.is_running = True
         self.scaled_surface = pygame.Surface(self.native_size)
         self.camera = Camera(self.native_size[0], self.native_size[1], self.level.width, self.level.height)
+
+    def load_player(self):
+        w, h = get_game_data("player_size")
+        s = get_game_data("player_scale")
+        x,y = self.level.spawn
+        return Player(x, y, w, h, s)
+
+    def load_next_level(self):
+        self.current_level += 1
+        self.level = Level(self.current_level)
+        self.camera = Camera(self.native_size[0], self.native_size[1], self.level.width, self.level.height)
+        self.player = self.load_player()
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -36,9 +54,11 @@ class GameEngine:
 
     def render(self):
         self.scaled_surface.fill((0, 0, 0))
-        self.background.render(self.scaled_surface, self.camera)
+        for background in self.backgrounds:
+            background.render(self.scaled_surface, self.camera)
         self.level.render(self.scaled_surface, self.camera)
         self.player.render(self.scaled_surface, self.camera)
+
         screen_width, screen_height = self.screen.get_size()
         scale_x = screen_width / self.native_size[0]
         scale_y = screen_height / self.native_size[1]
@@ -48,11 +68,13 @@ class GameEngine:
         scaled_surface = pygame.transform.scale(self.scaled_surface, (new_width, new_height))
         x_offset = (screen_width - new_width) // 2
         y_offset = (screen_height - new_height) // 2
+
         self.screen.fill((0, 0, 255))
         self.screen.blit(scaled_surface, (x_offset, y_offset))
 
     def update(self):
         self.player.update(self.level, 1 / self.fps)
+        self.level.check_touch(self.player, self)
         self.camera.follow(self.player)
 
     def run(self):
--- game/levels.py
+++ game/levels.py
@@ -1,8 +1,8 @@
 import json
 import os.path
-
 import pygame
 from core.game_data import get_game_data
+from game.tiles import BLOCK_TYPES, Block
 
 class Level:
     def __init__(self, level_number):
@@ -10,27 +10,29 @@ class Level:
             loc = json.load(f)
             self.blueprint = pygame.image.load(os.path.join("assets/levels",loc[str(level_number)])).convert_alpha()
         self.scale: int = get_game_data("level_scale")
-        self.tile_textures, self.collision_types = self.load_tiles()
+        self.tiles_data = self.load_tiles()
         self.gravity = 3
         self.tiles, self.tile_collisions = self.process_blueprint()
+        self.spawn = (100,400)
 
         self.height = len(self.tiles[0]) * self.scale
         self.width = len(self.tiles) * self.scale
 
     def load_tiles(self):
+        print(BLOCK_TYPES)
         with open("assets/tiles/tiles.json") as f:
             loc = json.load(f)
-        textures = {}
-        collision_types = {}
+
+        tile_data = {}
         for color, path in loc.items():
-            textures[color] = pygame.image.load(os.path.join("assets/tiles",path["texture"])).convert_alpha()
-            collision_types[color] = path["collision_type"]
-        return textures, collision_types
+            block_class = BLOCK_TYPES.get(path.get("type","block"), Block)
+            tile_data[color] = block_class(path["texture"], path["collision_type"], path.get("metadata",{}))
+
+        return tile_data
 
     def process_blueprint(self):
         w, h = self.blueprint.get_size()
         pixel_array = pygame.PixelArray(self.blueprint)
-
         tiles = []
         collisions = []
 
@@ -41,8 +43,9 @@ class Level:
                 color = self.blueprint.unmap_rgb(pixel_array[x, y])
                 color_key = f"{color[0]},{color[1]},{color[2]}" if color[3] == 255 else None
 
-                row_tiles.append(self.tile_textures.get(color_key, None))
-                row_collisions.append(self.collision_types.get(color_key, "air"))
+                tile_class = self.tiles_data.get(color_key, None)
+                row_tiles.append(tile_class)
+                row_collisions.append(tile_class.collision_type if tile_class else "air")
 
             tiles.append(row_tiles)
             collisions.append(row_collisions)
@@ -50,10 +53,20 @@ class Level:
         del pixel_array
         return tiles, collisions
 
+    def check_touch(self, player, engine):
+        if not player.on_ground:
+            return
+        tile_x = player.rect.centerx // self.scale
+        tile_y = player.rect.centery // self.scale
+
+        if 0 <= tile_x < len(self.tiles) and 0 <= tile_y < len(self.tiles[0]):
+            block = self.tiles[tile_x][tile_y]
+            if block and hasattr(block, "on_touch"):
+                block.on_touch(engine)
+
     def render(self, screen, camera):
         for x, row in enumerate(self.tiles):
             for y, tile in enumerate(row):
                 if tile:
                     tile_rect = pygame.Rect(x * self.scale, y * self.scale, self.scale, self.scale)
-                    screen.blit(pygame.transform.scale(tile, (self.scale, self.scale)),
-                                camera.apply(tile_rect))
+                    screen.blit(pygame.transform.scale(tile.texture, (self.scale, self.scale)), camera.apply(tile_rect))
