2ba0aef0cb1153a3d14e1c42c67a72e92fed79ad
CIonescu-jgu <cionescu@students.uni-mainz.de>
2025-03-25 15:58:10 +0100

    Boss Change
    Der Boss wurde leicht geändert und EMPs sollten jetzt funktionieren

--- game/enemies/emp_radar.py
+++ game/enemies/emp_radar.py
@@ -7,12 +7,12 @@ from game.entities import Entity
 class EMP_Radar(Entity):
     def __init__(self, x, y, level, player):
         super().__init__(x, y, "assets/characters/drone.png", "assets/characters/drone.json", level, None)
-        self.max_health = 1000
+        self.max_health = 1
         self.health = self.max_health
         self.level = level
         self.apply_gravity = False
-        self.lifetime = 5
-        self.range = 80
+        self.lifetime = 5.0
+        self.range = 200
         self.player = player
 
     def update(self, level, dt):
@@ -20,14 +20,14 @@ class EMP_Radar(Entity):
         if self.lifetime > 0:
             self.lifetime -= dt
         else:
-            self.eliminate
-        self.disable(dt)
+            self.eliminate()
+        self.disable()
 
     def eliminate(self):
         self.level.player.abilities_blocked = False
         super().eliminate()
 
-    def disable(self, dt):
+    def disable(self):
         distance = pygame.Vector2(self.rect.center).distance_to(self.player.rect.center)
         if distance <= self.range:
             self.player.abilities_blocked = True
